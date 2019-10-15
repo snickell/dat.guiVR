c79dceb38f036c01e648de7afea7bac9f5cf7737
Peter Todd <ptr.tdd@gmail.com>
2019-10-15 11:44:35 +0100

    sdfshader pruning

--- modules/datguivr/sdfshader_logdepth.js
+++ modules/datguivr/sdfshader_logdepth.js
@@ -14,7 +14,7 @@ const meshbasic_vert = `
 #define USE_MAP
 #include <common>
 #include <uv_pars_vertex>
-#include <color_pars_vertex>
+//#include <color_pars_vertex>
 #include <fog_pars_vertex>
 #include <logdepthbuf_pars_vertex>
 #include <clipping_planes_pars_vertex>
@@ -30,7 +30,6 @@ void main() {
 	#include <worldpos_vertex>
 	#include <clipping_planes_vertex>
 	#include <fog_vertex>
-
 }
 `;
 
@@ -39,18 +38,10 @@ const meshbasic_frag = `
 uniform vec3 color;
 uniform float opacity;
 
-#ifndef FLAT_SHADED
-
-	varying vec3 vNormal;
-
-#endif
-
 #include <common>
-#include <color_pars_fragment>
+//#include <color_pars_fragment>
 #include <uv_pars_fragment>
 #include <map_pars_fragment>
-#include <alphamap_pars_fragment>
-#include <lightmap_pars_fragment>
 #include <fog_pars_fragment>
 #include <logdepthbuf_pars_fragment>
 #include <clipping_planes_pars_fragment>
@@ -87,39 +78,6 @@ void main() {
 }
 `;
 
-const sdf_vert = `
-attribute vec2 uv;
-attribute vec4 position;
-uniform mat4 projectionMatrix;
-uniform mat4 modelViewMatrix;
-varying vec2 vUv;
-void main() {
-    vUv = uv;
-    gl_Position = projectionMatrix * modelViewMatrix * position;
-}`;
-const sdf_frag = `
-#ifdef GL_OES_standard_derivatives
-#extension GL_OES_standard_derivatives : enable
-#endif
-precision highp float;
-uniform float opacity;
-uniform vec3 color;
-uniform sampler2D map;
-varying vec2 vUv;
-float aastep(float value) {
-    #ifdef GL_OES_standard_derivatives
-        float afwidth = length(vec2(dFdx(value), dFdy(value))) * 0.70710678118654757;
-    #else
-        float afwidth = (1.0 / 32.0) * (1.4142135623730951 / (2.0 * gl_FragCoord.w));
-    #endif
-    return smoothstep(0.5 - afwidth, 0.5 + afwidth, value);
-}
-void main() {
-    vec4 texColor = texture2D(map, vUv);
-    float alpha = aastep(texColor.a);
-    gl_FragColor = vec4(color, opacity * alpha);
-    if (gl_FragColor.a < 0.0001) discard;
-}`;
 
 module.exports = function createSDFShader (opt) {
   opt = opt || {};
@@ -130,8 +88,9 @@ module.exports = function createSDFShader (opt) {
   var color = opt.color;
   var map = opt.map;
   
-  const experiment = true; //nb, for false condition to work, need to change sdftext back to using RawShader
-  opt.flatShading = true; //to force GL_OES_standard_derivatives in pre-processor, could use #define instead?
+  opt.extensions = opt.extensions || {};
+  opt.extensions.derivatives = true; //nb, false defaults for fragDepth, drawBuffers, shaderTextureLOD will no longer be there.
+  opt.name = "SDF text material";
 
   // remove to satisfy r73
   delete opt.map;
@@ -145,7 +104,7 @@ module.exports = function createSDFShader (opt) {
       map: { type: 't', value: map || new THREE.Texture() },
       color: { type: 'c', value: new THREE.Color(color) }
     },
-    vertexShader: experiment ? meshbasic_vert : sdf_vert,
-    fragmentShader: experiment ? meshbasic_frag : sdf_frag
+    vertexShader: meshbasic_vert,
+    fragmentShader: meshbasic_frag
   }, opt);
 }
